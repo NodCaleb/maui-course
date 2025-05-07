a93db844ccee3605e48b41b1f22956d0a8109bc8
Eugene Rozhkov <eugene.rozhkov@betssongroup.com>
2025-05-07 14:46:49 +0300

    Pages demo

---
+++ MauiAppOne/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppOne
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PagesDemo/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace PagesDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var navPage = new NavigationPage(new MainPage());
+
+            navPage.BarBackground = Colors.Chocolate;
+            navPage.BarTextColor = Colors.White;
+
+            return new Window(new TabbedPageDemo());
+        }
+    }
+}
