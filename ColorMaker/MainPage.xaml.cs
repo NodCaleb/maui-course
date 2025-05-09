4ac7d330f770be680c053eb9708c879cf8f280ae
Eugene Rozhkov <eugene.rozhkov@betssongroup.com>
2025-05-09 13:31:20 +0300

    Color maker

---
+++ ColorMaker/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace ColorMaker
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string hexColor = string.Empty;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColor(color);
+        }
+
+        private void SetColor(Color color)
+        {
+            btnRandom.BackgroundColor = color;
+            Container.BackgroundColor = color;
+            hexColor = color.ToHex();
+            lblHex.Text = color.ToHex();
+        }
+
+        private void btnRandom_Clicked(object sender, EventArgs e)
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+
+            var color = Color.FromRgb(
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256)
+                );
+
+            SetColor(color);
+
+            sldRed.Value = color.Red;
+            sldGreen.Value = color.Green;
+            sldBlue.Value = color.Blue;
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(hexColor);
+            var toast = Toast.Make($"Color {hexColor} copied to clipboard",
+                ToastDuration.Short,
+                14
+                );
+            await toast.Show();
+        }
+    }
+
+}
