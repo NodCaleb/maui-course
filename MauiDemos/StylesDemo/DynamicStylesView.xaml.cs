06f949e0a5caf0307fb2757a7efe5de4239bcb64
Eugene Rozhkov <eugene.rozhkov@betssongroup.com>
2025-05-28 15:47:02 +0300

    Styles

---
+++ MauiDemos/StylesDemo/DynamicStylesView.xaml.cs
@@ -0,0 +1,17 @@
+namespace StylesDemo;
+
+public partial class DynamicStylesView : ContentPage
+{
+	public DynamicStylesView()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Application.Current.Resources.TryGetValue("specialButton", out var retVal);
+
+		//Resources["dynStyle"] = Resources["greenStyle"];
+		Resources["dynStyle"] = (Style)retVal;
+    }
+}
