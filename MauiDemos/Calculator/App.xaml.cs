6ff9e369435cf8749588f59e8c923e41a3cc2743
Eugene Rozhkov <eugene.rozhkov@betssongroup.com>
2025-05-29 13:04:36 +0300

    Calculator

---
+++ MauiDemos/Calculator/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Calculator.MVVM;
+
+namespace Calculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new CalcView());
+        }
+    }
+}
---
+++ MauiDemos/Calculator/MVVM/ViewModels/CalcViewModel.cs
@@ -0,0 +1,54 @@
+﻿
+using PropertyChanged;
+using System.Windows.Input;
+
+namespace Calculator.MVVM.ViewModels;
+
+[AddINotifyPropertyChangedInterface]
+public class CalcViewModel
+{
+    public string Formula { get; set; }
+    public string Result { get; set; } = "0";
+
+    public ICommand OperationCommand =>
+        new Command((number) =>
+        {
+            Formula += number;
+        });
+
+    public ICommand ResetCommand =>
+        new Command(() =>
+        {
+            Formula = string.Empty;
+            Result = "0";
+        });
+
+    public ICommand BackspaceCommand =>
+        new Command(() =>
+        {
+            if (Formula.Length > 0)
+            {
+                Formula = Formula.Remove(Formula.Length - 1);
+            }
+        });
+
+    public ICommand CalculateCommand =>
+        new Command(() =>
+        {
+            if (string.IsNullOrEmpty(Formula))
+            {
+                Result = "0";
+                return;
+            }
+
+            try
+            {
+                var result = Dangl.Calculator.Calculator.Calculate(Formula);
+                Result = result.Result.ToString();
+            }
+            catch
+            {
+                Result = "Error";
+            }
+        });
+}
